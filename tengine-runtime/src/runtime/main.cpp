10f62b49f5c0edab47d59ff7c894a2148cc78e9f
DIYLabsED <DIYLabsED@users.noreply.github.com>
2026-05-27 11:41:27 +0530

    fix: remove erroneous FMT include in runtime main.cpp
    Signed-off-by: DIYLabsED <DIYLabsED@users.noreply.github.com>

--- tengine-runtime/src/runtime/main.cpp
+++ tengine-runtime/src/runtime/main.cpp
@@ -3,11 +3,8 @@
 #include <runtime/core/init.hpp>
 #include <runtime/core/RuntimeGlobals.hpp>
 
-#include "../../../cmake-build-demo/_deps/fmt-src/include/fmt/xchar.h"
-
 
 truntime::RuntimeGlobals runtimeGlobals;
-
 
 
 int main(int argc, char* argv[]){
